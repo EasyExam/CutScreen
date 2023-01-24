bf54381158ab0ad54e23a745bde66a0870fa75a9
EasyExam <121533620+EasyExam@users.noreply.github.com>
2023-01-24 18:12:38 +0800

    Add files via upload

--- CutScreen/Properties/AssemblyInfo.cs
+++ CutScreen/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("CutScreen")]
-[assembly: AssemblyDescription("EasyExam666")]
+[assembly: AssemblyDescription("EasyExam")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("截屏")]
